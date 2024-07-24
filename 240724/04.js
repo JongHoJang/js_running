b2afb5c30c82db92d612e79aa7de147ea5f62d55
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2024-07-24 12:25:03 +0900

    5번 문제 완료

--- 240724/04.js
+++ 240724/04.js
@@ -2,21 +2,21 @@
 // 연산자와 함수, 조건문을 토대로 계산기 함수를 하나 만들어 보려고 합니다.
 // 함수에 숫자, 연산자, 숫자 세 개의 매개변수를 넣었을 때 해당 연산자를 기준으로 연산을 해서 값을 반환하는 함수를 만들어주세요.
 
-function 함수명(매개변수1, 매개변수2, 매개변수3) {
+function calculate(num1, operator, num2) {
   let result;
-  if (매개변수2 === "+") {
-    result = 매개변수1 + 매개변수3;
-  } else if (매개변수2 === "-") {
-    result = 매개변수1 - 매개변수3;
-  } else if (매개변수2 === "*") {
-    result = 매개변수1 * 매개변수3;
-  } else if (매개변수2 === "/") {
-    result = 매개변수1 / 매개변수3;
+  if (operator === "+") {
+    result = num1 + num2;
+  } else if (operator === "-") {
+    result = num1 - num2;
+  } else if (operator === "*") {
+    result = num1 * num2;
+  } else if (operator === "/") {
+    result = num1 / num2;
   }
   console.log(result);
 }
 
-함수명(3, "+", 6); // 결과값 9
-함수명(11, "-", 6); // 결과값 5
-함수명(6, "*", 3); // 결과값 18
-함수명(15, "/", 3); // 결과값 5
+calculate(3, "+", 6); // 결과값 9
+calculate(11, "-", 6); // 결과값 5
+calculate(6, "*", 3); // 결과값 18
+calculate(15, "/", 3); // 결과값 5
--- 240724/05.js
+++ 240724/05.js
@@ -3,15 +3,28 @@
 // scores에 있는 학생들의 점수를 반복문을 통해 3점씩 올리게 고쳐주시는데 4번 문제를 통해 만든 계산기 함수를 통해 더해주세요.
 
 const scores = [36, 62, 72, 55, 86, 95, 92, 48, 81];
-let newScore = [];
 
-function 함수명(scores) {
-  for (let i = 0; i < scores.length; i++) {
-    newScore.push(scores[i] + 3);
+// 문제 4의 계산기
+function calculate(num1, operator, num2) {
+  let result;
+  if (operator === "+") {
+    result = num1 + num2;
+  } else if (operator === "-") {
+    result = num1 - num2;
+  } else if (operator === "*") {
+    result = num1 * num2;
+  } else if (operator === "/") {
+    result = num1 / num2;
   }
-  return newScore;
+  return result;
 }
 
-console.log(newScore);
+const num2 = 3;
+const operator = "+";
+
+for (let i = 0; i < scores.length; i++) {
+  let result = calculate(scores[i], operator, num2);
+  console.log(result);
+}
 
 // 기대값: [39, 65, 75, 58, 89, 98, 95, 51, 84]
